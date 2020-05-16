48629f29f6c03d0aaa236b816247db6a11c0c39c
Ilamah, Osho <oilamah@gmail.com>
2020-05-16 09:48:43 +0300

    Completed demo backend, with dummy GET /test endpoint

--- backend/src/main/java/com/munoor/demo/DemoApplication.java
+++ backend/src/main/java/com/munoor/demo/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
